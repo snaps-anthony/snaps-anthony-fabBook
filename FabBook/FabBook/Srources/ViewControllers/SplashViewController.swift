65c1c57769ab3c82faaaabcebcad9b745cbe093f
anthonySnaps <anthony.lee@snaps.com>
2021-12-08 13:02:20 +0900

    basic setting

---
+++ FabBook/FabBook/Srources/ViewControllers/SplashViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SplashViewController.swift
+//  FabBook
+//
+//  Created by anthony on 2021/12/08.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
---
+++ FabBook/Pods/Target Support Files/Pods-FabBook-FabBookUITests/Pods-FabBook-FabBookUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FabBook_FabBookUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FabBook_FabBookUITests
+@end
